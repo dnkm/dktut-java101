63e3cb8892c5bb8ff61291ee19502d01ea9def38
Daniel Kim <chipperday@gmail.com>
2016-10-08 18:05:06 -0700

    sorting algorithms

--- src/com/dktut/java201/sorting/InsertionSort.java
+++ src/com/dktut/java201/sorting/InsertionSort.java
@@ -2,25 +2,31 @@ package com.dktut.java201.sorting;
 
 import com.dktut.java101.array.ArrayUtils;
 
+/*
+ * in-place selection sort
+ */
 public class InsertionSort {
 	public static void main(String[] args) {
 		int[] arr = { 2, 7, 4, 1, 5, 3 };
 		insertionSort(arr);
 	}
-
+	
 	private static void insertionSort(int[] arr) {
-		for (int i = 1; i < arr.length; i++) {
-			shiftLeftUntilSorted(arr, i);
+		for (int i = 0; i < arr.length - 1; i++) {
+			int minIndex = findMinIndex(arr, i);
+			ArrayUtils.swap(arr, minIndex, i);
 
 			ArrayUtils.printArray(arr);
 		}
 	}
 
-	private static void shiftLeftUntilSorted(int[] arr, int i) {
-		for (int j = i; j > 0; j--) {
-			if (arr[j] > arr[j - 1])
-				break;
-			ArrayUtils.swap(arr, j, j - 1);
+	private static int findMinIndex(int[] arr, int startingIndex) {
+		int minIndex = startingIndex;
+		for (int i = minIndex; i < arr.length; i++) {
+			if (arr[i] < arr[minIndex])
+				minIndex = i;
 		}
+		return minIndex;
 	}
+
 }
--- src/com/dktut/java201/sorting/SelectionSort.java
+++ src/com/dktut/java201/sorting/SelectionSort.java
@@ -2,31 +2,25 @@ package com.dktut.java201.sorting;
 
 import com.dktut.java101.array.ArrayUtils;
 
-/*
- * in-place selection sort
- */
 public class SelectionSort {
 	public static void main(String[] args) {
 		int[] arr = { 2, 7, 4, 1, 5, 3 };
 		selectionSort(arr);
 	}
-	
+
 	private static void selectionSort(int[] arr) {
-		for (int i = 0; i < arr.length - 1; i++) {
-			int minIndex = findMinIndex(arr, i);
-			ArrayUtils.swap(arr, minIndex, i);
+		for (int i = 1; i < arr.length; i++) {
+			shiftLeftUntilSorted(arr, i);
 
 			ArrayUtils.printArray(arr);
 		}
 	}
 
-	private static int findMinIndex(int[] arr, int startingIndex) {
-		int minIndex = startingIndex;
-		for (int i = minIndex; i < arr.length; i++) {
-			if (arr[i] < arr[minIndex])
-				minIndex = i;
+	private static void shiftLeftUntilSorted(int[] arr, int i) {
+		for (int j = i; j > 0; j--) {
+			if (arr[j] > arr[j - 1])
+				break;
+			ArrayUtils.swap(arr, j, j - 1);
 		}
-		return minIndex;
 	}
-
 }
